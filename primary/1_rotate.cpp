1d25c563ebda6b22df4e494d91339e2491d0512c
gggmt <1065504814@qq.com>
2018-12-13 13:04:03 +0600

    旋转数组

---
+++ primary/1_rotate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int size = nums.size();
+        vector<int> result(size,0);
+        for(int i=0; i<size; i++)
+        {
+            result[(i+k) % size] = nums[i];
+        }
+        nums = result;
+    }
+};
