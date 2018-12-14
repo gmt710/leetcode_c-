0c1f0dd95522261503759380c268f1968bdb4887
gggmt <1065504814@qq.com>
2018-12-14 12:21:17 +0600

    存在重复

---
+++ primary/1_containsDuplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<size-1; i++){
+            if(nums[i+1] == nums[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+};
