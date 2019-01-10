7bb5fd18495ca2df5d14dd3e43d35f55c7c214df
gggmt <1065504814@qq.com>
2019-01-10 16:03:32 +0800

    验证二叉搜索树

---
+++ primary/4_isValidBST.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+static int x = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+class Solution {
+public:
+    bool ValidBST(TreeNode* root, long small,long large){
+        if(root == NULL)
+            return true;
+        if((small >= root->val) || (large <= root->val))
+            return false;
+        return ValidBST(root->left, small, root->val) && ValidBST(root->right, root->val, large);
+    }
+    bool isValidBST(TreeNode* root) {
+        // 此处使用 LONG_MIN, LONG_MAX
+        return ValidBST(root, LONG_MIN, LONG_MAX);
+    }
+};
