a412551426975cb81f5fc4f6a6b8d30f6d90760d
gggmt <1065504814@qq.com>
2019-01-11 15:33:18 +0800

    二叉树的层次遍历

--- primary/4_levelOrder.cpp
+++ primary/4_levelOrder.cpp
@@ -15,6 +15,8 @@ static int x = [](){
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        // python 68ms
+        // c++ 4ms
         // 参考：https://blog.csdn.net/Jaster_wisdom/article/details/80977829
         // 采用BFS：
         // BFS：
@@ -24,16 +26,21 @@ public:
         // 则依次访问Vi1,Vi2,Vi3...Viy未访问过的邻接点
         // 4.重复3，直到所有顶点均被访问
         
+        // 初始化一个返回值的类型
         vector<vector<int>> ans;
+        // 初始化一个队列来存储
         queue<TreeNode*> q;
         if(root == NULL)
             return ans;
+        // 将root入队
         q.push(root);
         while(!q.empty()){
+            // 每一层中的所有结点
             vector<int> temp;
+            // 每一层的大小
             int length = q.size();
             for(int i=0; i < length; i++){
-                // 队尾出栈
+                // 队尾出队
                 TreeNode* node = q.front();
                 temp.push_back(node->val);
                 q.pop();
