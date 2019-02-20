d2cd98447ad3c692f6c8450a369a39754f8e9b8a
gggmt <1065504814@qq.com>
2019-02-20 14:32:17 +0800

    Create 1_threeSum.cpp

---
+++ middle/1_threeSum.cpp
@@ -0,0 +1,46 @@
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // python:800ms
+        // c++:160ms
+        vector<vector<int>> result;
+        if (nums.size() < 3)
+        {
+            return result;
+        }
+        
+        std::sort(nums.begin(), nums.end());
+        for (size_t i = 0; i < nums.size(); ++i)
+        {   
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            
+            size_t start = i + 1;
+            size_t end = nums.size() - 1;
+            while (start < end)
+            {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (sum == 0)
+                {
+                    while (start < end && nums[start] == nums[start+1]) ++start;
+                    while (start < end && nums[end] == nums[end-1]) --end;
+                    result.push_back({nums[i], nums[start], nums[end]});
+                    ++start;
+                    --end;
+                }
+                else if (sum < 0)
+                {
+                    ++start;
+                }
+                else if (sum > 0)
+                {
+                    --end;
+                }
+            }
+        }
+        return result;
+    }
+};
